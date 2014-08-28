ceb46b10ee975869f75beef258aad880fdeb5ce5
Larry O'Brien <lobrien@knowing.net>
2014-08-27 16:12:49 -1000

    Initial commit

---
+++ FSXF1/AppDelegate.fs
@@ -0,0 +1,38 @@
+﻿namespace FSXF1
+
+open System
+open MonoTouch.UIKit
+open MonoTouch.Foundation
+open Xamarin.Forms
+
+type App = class
+    static member GetMainPage = 
+        let lbl = new Label()
+        lbl.Text <- "Hello, F# Xam.Forms!"
+        lbl.VerticalOptions <- LayoutOptions.CenterAndExpand
+        lbl.HorizontalOptions <- LayoutOptions.CenterAndExpand
+
+        let cp = new ContentPage()
+        cp.Content <- lbl
+        cp
+
+end
+
+[<Register("AppDelegate")>]
+type AppDelegate() = 
+    inherit UIApplicationDelegate()
+    member val Window = null with get, set
+    // This method is invoked when the application is ready to run.
+    override this.FinishedLaunching(app, options) = 
+        this.Window <- new UIWindow(UIScreen.MainScreen.Bounds)
+        Forms.Init()
+        this.Window.RootViewController <- App.GetMainPage.CreateViewController()
+        this.Window.MakeKeyAndVisible()
+        true
+
+module Main = 
+    [<EntryPoint>]
+    let main args = 
+        UIApplication.Main(args, null, "AppDelegate")
+        0
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# "Hello, Xamarin.Forms" in F#
+# "Hello, Xamarin.Forms" in F\#
 
 Accompanies [http://www.knowing.net/index.php/2014/08/27/xamarin-forms-programming-in-f/](http://www.knowing.net/index.php/2014/08/27/xamarin-forms-programming-in-f/)
